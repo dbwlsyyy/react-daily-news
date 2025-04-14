896b0d95a6120700e02b9831751ded1f7685dcde
홍유진 <dbwlsyyy@naver.com>
2025-04-15 03:37:09 +0900

    파이어베이스 api 키 재발급 후 .env로 분리

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -9,7 +9,7 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyBjcZQZRje2JqN9F4WW_mF466-nOPkeB6s',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: 'daily-news-a8a69.firebaseapp.com',
     projectId: 'daily-news-a8a69',
     storageBucket: 'daily-news-a8a69.firebasestorage.app',
